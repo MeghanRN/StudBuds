060fb6f7220512983744f3e841039cd2c4f7c184
megvo <112580071+megvo@users.noreply.github.com>
2025-03-17 01:00:02 -0400

    Update UserRepository.java
    added more suggestions

--- backend/src/main/java/com/studbuds/repository/UserRepository.java
+++ backend/src/main/java/com/studbuds/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); // ⚠️ we should add existsByEmail(string Email) for faster email existence checks, rather than fetching the entire user object.
+    // ⚠️ we should also make sure emails aren't case sensitive
+    // ⚠️ should we also add a method to search users by their name? 
 }
